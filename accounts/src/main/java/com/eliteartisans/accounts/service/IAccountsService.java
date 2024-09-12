90b672cc473315e241c98c494744aafcb5d71b15
ahmedsaifi756 <ahmedsaifi756@gmail.com>
2024-09-12 15:42:53 +0530

    added update api inside accounts microservices

--- accounts/src/main/java/com/eliteartisans/accounts/service/IAccountsService.java
+++ accounts/src/main/java/com/eliteartisans/accounts/service/IAccountsService.java
@@ -5,7 +5,6 @@ import com.eliteartisans.accounts.dto.CustomerDto;
 public interface IAccountsService {
 	
 	/**
-	 * 
 	 * @param customerDto- CustomerDto Object
 	 */
 	void createAccount(CustomerDto customerDto);
@@ -15,4 +14,9 @@ public interface IAccountsService {
 	 * @return Accounts details based on mobile Number
 	 */
 	CustomerDto fetchAccount(String mobileNumer);
+	/**
+	 *  @param customerDto input customer DTO details
+	 * @return true/false based on requirement
+	 */	
+	boolean updateAccount(CustomerDto customerDto);
 }
